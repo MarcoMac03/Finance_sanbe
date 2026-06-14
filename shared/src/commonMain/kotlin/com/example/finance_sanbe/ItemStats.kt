3fcd0c2ef49384892af2dd0c742df482c0f18c0a
Marco <marco.nardi@edu.unito.it>
2026-06-14 17:55:40 +0200

    Fixing last problems

--- shared/src/commonMain/kotlin/com/example/finance_sanbe/ItemStats.kt
+++ shared/src/commonMain/kotlin/com/example/finance_sanbe/ItemStats.kt
@@ -1,14 +1,18 @@
 package com.example.finance_sanbe
 
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ItemStats (
+data class ItemStats @OptIn(ExperimentalSerializationApi::class) constructor(
     val id: Int = -1,
     val name: String = "",
     val initialPrice: Int? = null,
     val actualPrice: Int = -1,
     val initialQuantity: Int? = null,
     val actualQuantity: Int = -1,
+
+    @EncodeDefault
     val maxPrice: Int? = null
 )
